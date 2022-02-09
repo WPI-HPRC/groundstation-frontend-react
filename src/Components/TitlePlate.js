ac139c27d40c72f3cf02e93370e3d7bb695b94ab
Ted Clifford <efclifford@wpi.edu>
2022-02-08 21:51:43 -0500

    Changed title plate text placement

--- src/Components/TitlePlate.js
+++ src/Components/TitlePlate.js
@@ -6,8 +6,8 @@ function TitlePlate (props) {
     return (
         <div className="panel">
             <div className={props.className}>
-                <h1>WPI <img src="hprc logo red.png" style={{height: "4vh", width: "3vh"}}/> HPRC</h1>
-                <h4>Aquila GS</h4>
+                <h1 className="title">WPI <img src="hprc logo red.png" style={{height: "4vh", width: "3vh"}}/> HPRC</h1>
+                <h4 className="subTitle">Aquila GS</h4>
             </div>
         </div>
     );
