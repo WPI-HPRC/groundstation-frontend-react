617102ea862251dd55c838ebc852b4d6640015c8
Ted Clifford <efclifford@wpi.edu>
2022-02-09 19:50:15 -0500

    Restored splashscreen

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ export default class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            renderSplashScreen: false,
+            renderSplashScreen: true,
             battery: "-",
             temperature: "-",
             stateStr: "Apogee",
