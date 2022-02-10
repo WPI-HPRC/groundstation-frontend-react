ced15235c03803eb2e4111f2b87133a35c72eca6
frostydev <dpear0915@gmail.com>
2022-02-10 10:42:58 -0500

    Added map but not offline

--- src/Components/Visualization.js
+++ src/Components/Visualization.js
@@ -1,15 +1,39 @@
 import React from 'react';
 
+import Map, {Marker} from 'react-map-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
+import '../map.css';
+import {Map, Marker, Popup, TileLayer} from 'react-leaflet';
 
-function Visualization (props) {
-
-    return (
-        <div className="panel">
-            <div className={props.className}>
-                <h3>Map   |    3D</h3>
-            </div>
-        </div>
-    );
-}
-
-export default Visualization;
+export default class Visualization extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            lat: 42.2741403879473,
+            long: -71.80824475604742,
+            zoom: 10,
+            maxZoom: 30
+        }
+    }
+    
+    render() {
+        let pos = [this.state.lat, this.state.long]
+        return (
+            <>
+                <div className='panel' style={{width: "100vw", height: "44vh"}}>
+                    <Map
+                        mapboxAccessToken='pk.eyJ1IjoiZGpwZWFyc29uIiwiYSI6ImNremcwdTE0NzNlYmMycnByczFtbTJ1N3EifQ.Bhsb7qBWXmjz5gXfH7Mv0A'
+                        initialViewState={{
+                        longitude: this.state.long,
+                        latitude: this.state.long,
+                        zoom: 3.5
+                        }}
+                        style={{width: "100%", height: "100%"}}
+                        mapStyle="mapbox://styles/mapbox/streets-v9"
+                    />
+                </div>
+            </>
+            
+        );
+    }
+};
