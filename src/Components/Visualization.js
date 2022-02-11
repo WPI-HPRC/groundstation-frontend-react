1853f2853974697a25503cb2174997a40ac6068b
Ted Clifford <efclifford@wpi.edu>
2022-02-11 18:48:27 -0500

    React leaflet

--- src/Components/Visualization.js
+++ src/Components/Visualization.js
@@ -1,12 +1,8 @@
-import React, { useEffect, useState } from 'react';
-import L, {Map} from 'leaflet';
+import React from 'react';
 import 'leaflet.offline';
-import {MapContainer, Tile, Marker, Popup, TileLayer} from 'react-leaflet';
-import {localforage} from 'localforage';
+import {MapContainer, TileLayer} from 'react-leaflet';
 import 'leaflet/dist/leaflet.css'
 
-
-var map;
 
 export default class Visualization extends React.Component {
     constructor(props) {
@@ -18,6 +14,8 @@ export default class Visualization extends React.Component {
             map: null,
             dark: props.dark
         }
+
+        this.map = 0;
     }
 
     static getDerivedStateFromProps(props, current_state) {
@@ -30,12 +28,7 @@ export default class Visualization extends React.Component {
     }
 
     componentDidMount() {
-        map = L.map('map').setView({lon: 0, lat: 0}, 2);
-        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 50,
-        }).addTo(map);
-        //Center on WPI
-        map.setView([42.27470400012541,-71.80915315792504], 15);
+
     }
 
     componentDidUpdate() {
@@ -43,14 +36,33 @@ export default class Visualization extends React.Component {
     }
 
     render() {
+
+        var position = [42.27470400012541,-71.80915315792504];
         
         return (
             <>
                 <div className={`panel ${this.state.dark ? "darkPanel" : "lightPanel"}`}>
-                    <div id="map" style={{width:"100%", height: "100%"}}>
+                    <MapContainer center={position} zoom={13} scrollWheelZoom={true}>
+                        <TileLayer
+                            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                        />
+                    </MapContainer>
+                    {/* <div id="map" style={{width:"100%", height: "100%"}}>
 
-                    </div>
+                    </div> */}
                 </div>
+                <style jsx> {`
+                    .leaflet-container {
+                        width: 100%; 
+                        height: 100%;
+                      }
+
+
+                
+                `}
+
+                </style>
             </>
         );
     }
